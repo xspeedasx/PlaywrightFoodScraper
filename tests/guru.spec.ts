83af6468cbdd61982e224e711d928038aaa3f832
Gedas Valuntis <gedasv@unifysquare.com>
2023-03-09 11:28:48 +0200

    guru adapt to dynamic width images

--- tests/guru.spec.ts
+++ tests/guru.spec.ts
@@ -7,14 +7,26 @@ test('open Guru fb', async ({page}) => {
     await page.waitForTimeout(2000);
 
     const acceptCookies = page.getByText("Allow essential and optional cookies").nth(1);
-    //await expect(acceptCookies).toContainText("cookies")
+    
     if(await acceptCookies.isVisible())
         await acceptCookies.click({button: 'left'})
 
-    const url = await page.$eval('img[src*=scontent][width="526"]', img => img.attributes.getNamedItem("src")?.value)
-    await page.goto(url!)
+    // find first image with >500 width
+    let allImages = await page.locator('img[src*=scontent]').all();
+    for(let img of allImages)
+    {
+        let w = +(await img.getAttribute("width"));
+        if(w > 500)
+        {
+            const url = await img.getAttribute("src");
+            await page.goto(url!)
 
-    await page
-        .locator("img")
-        .screenshot({path: "scraped/guru.png"});
+            await page
+                .locator("img")
+                .screenshot({path: "scraped/guru.png"});
+            return;
+        }
+    }
+    
+    
 });
